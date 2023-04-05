f7ecad5fffa50cc4102673eecb748e5772290d75
Forrest Jones <forrestjones2010@gmail.com>
2023-04-04 23:28:10 -0500

    14/15=93.67phase4A

--- server/models.py
+++ server/models.py
@@ -11,3 +11,45 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = 'pizzas'
+
+    serialize_rules = ('-restaurants.restaurant_pizzas', '-restaurant_pizzas.pizza')
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    pizza_restaurants = db.relationship('RestaurantPizza', backref = 'pizza')
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+
+    serialize_rules = ('-pizzas.pizza_restaurants', '-restaurant_pizzas.restaurant')
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref = 'restaurant')
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+
+    serialize_rules = ('-pizza.pizza_restaurants', '-restaurant.restaurant_pizzas')
+
+    id = db.Column(db.Integer, primary_key = True)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+
+    @validates('price')
+    def validates_price(self, key, value):
+        if value < 1 or value > 30:
+            raise ValueError("Price must be betweeen 1 and 30, exclusive!")
+        return value
